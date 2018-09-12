990e65f3f2319b3f393b325514ae3b90ead6ffe1
nellson.maia@gmail.com <nellson.maia@gmail.com>
2018-09-12 20:31:48 +0100

    Verifying login when the application is loaded
    Verify if there is a refresh token and if its possible to renew the access token with it, in case it is possible refresh the access token

--- 00-SSO/Auth0Sample/AppDelegate.swift
+++ 00-SSO/Auth0Sample/AppDelegate.swift
@@ -26,40 +26,74 @@ import Auth0
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let verifyRenewToken = DispatchGroup()
+        verifyRenewToken.enter()
+        
+        
+        SessionManager.shared.renewAuth { error in
+            
+            SessionManager.shared.retrieveProfile { error in
+                print(error)
+                if error != nil  {
+                    print("Failed to retrieve profile check token in Home: \(String(describing: error))")
+                    // return callback()
+                    SessionManager.shared.credentials = nil
+                    
+                }else{
+                    print("profile retrieved")
+                }
+                verifyRenewToken.leave()
+            }
+            print("waited 2")
+        }
+        
+        verifyRenewToken.wait()
+        
+        let id = SessionManager.shared.credentials == nil ? "Home" : "WelcomeNavigation"
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let exampleVC: UIViewController = mainStoryboard.instantiateViewController(withIdentifier: id) as UIViewController
+        self.window?.rootViewController = exampleVC
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
+    
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    
     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool {
         return Auth0.resumeAuth(url, options: options)
     }
-
-
+    
+    
 }
